59b80002cd86fe727d6b40d44f753cdcfed48089
kate_gaw <ekaterinagaw@gmail.com>
2020-04-26 17:00:34 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,107 @@
+const server = 'http://localhost:7070';
+const images = document.querySelector('.images');
+const error = document.getElementById('error-message');
+const selectBtn = document.querySelector('#selectBtn');
+
+const drawImages = (img, url) => {
+  const newImg = document.createElement('img');
+  newImg.className = 'img_el';
+  newImg.src = url;
+
+  newImg.addEventListener('load', () => {
+    error.classList.add('visibile');
+    const newElem = document.createElement('div');
+    newElem.className = 'image-element';
+    newElem.innerHTML = '<div class="delBtn">✗</div>';
+    newElem.appendChild(newImg);
+    images.appendChild(newElem);
+  });
+
+  newImg.addEventListener('error', () => {
+    error.classList.remove('visibile');
+  });
+};
+
+function loadFile(files) {
+  for (const item of files) {
+    const formData = new FormData();
+    formData.append('file', item);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', `${server}`);
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const urlImg = `${server}/${xhr.response}`;
+        drawImages('nameImg', urlImg);
+      }
+    });
+    xhr.send(formData);
+  }
+}
+
+
+const dnd = document.querySelector('#dnd');
+
+// add picture for DnD
+const dndAdding = () => {
+  dnd.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+
+  dnd.addEventListener('dragleave', (event) => {
+    event.preventDefault();
+  });
+
+  dnd.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const pic = Array.from(event.dataTransfer.files);
+    loadFile(pic);
+  });
+};
+dndAdding();
+
+
+// add picture for clicking
+dnd.addEventListener('click', () => {
+  selectBtn.value = null;
+  selectBtn.dispatchEvent(new MouseEvent('click'));
+});
+
+// show picture on the page
+selectBtn.addEventListener('input', (event) => {
+  const pic = Array.from(event.currentTarget.files);
+  loadFile(pic);
+});
+
+// delete picture
+images.addEventListener('click', (event) => {
+  if (event.target.className === 'delBtn') {
+    const img = event.target.closest('.image-element');
+    const params = new URLSearchParams();
+    params.append('file', img.querySelector('.img_el').src);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', `${server}/?${params}`);
+    xhr.addEventListener('load', () => {
+      console.log(xhr.responseText);
+    });
+    xhr.send();
+    img.parentNode.removeChild(img);
+  }
+});
+
+
+async function reloadSavedImages() {
+  await fetch(server, {
+    method: 'GET',
+  }).then(async (response) => {
+    const allImages = await response.json();
+    for (const img of allImages) {
+      if (img !== '.getkeep') {
+        drawImages('nameImg', img);
+      }
+    }
+  }).catch((err) => {
+    console.error(err);
+  });
+}
+reloadSavedImages();
---
+++ src/js/server.js
@@ -0,0 +1,116 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-return-await */
+/* eslint-disable consistent-return */
+
+const http = require('http');
+const path = require('path');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const koaStatic = require('koa-static');
+const fs = require('fs');
+
+const app = new Koa();
+
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback()).listen(port);
+
+// Static file handling
+const imgFolder = path.join(__dirname, '/imgFolder');
+app.use(koaStatic(imgFolder));
+
+// CORS
+app.use(async (ctx, next) => {
+  const origin = ctx.request.get('Origin');
+  if (!origin) {
+    return await next();
+  }
+  const headers = { 'Access-Control-Allow-Origin': '*' };
+
+  if (ctx.request.method !== 'OPTIONS') {
+    ctx.response.set({ ...headers });
+    try {
+      return await next();
+    } catch (e) {
+      e.headers = { ...e.headers, ...headers };
+      throw e;
+    }
+  }
+
+  if (ctx.request.get('Access-Control-Request-Method')) {
+    ctx.response.set({
+      ...headers,
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+    });
+
+    if (ctx.request.get('Access-Control-Request-Headers')) {
+      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
+    }
+    ctx.response.status = 204;
+  }
+});
+
+// Body Parsers
+app.use(koaBody({
+  text: true,
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
+
+// kod
+let listImgs = [];
+
+app.use(async (ctx) => {
+  // GET
+  if (ctx.method === 'GET') {
+    const images = [];
+    listImgs.forEach((item) => {
+      images.push(item);
+    });
+    ctx.response.body = JSON.stringify(images);
+  }
+
+  // POST
+  if (ctx.method === 'POST') {
+    const { name } = ctx.request;
+    const { file } = ctx.request.files;
+    const link = await new Promise((resolve, reject) => {
+      const oldPath = file.path;
+      const filename = file.name;
+      const newPath = path.join(imgFolder, filename);
+
+      const callback = (error) => reject(error);
+
+      const readStream = fs.createReadStream(oldPath);
+      const writeStream = fs.createWriteStream(newPath);
+
+      readStream.on('error', callback);
+      writeStream.on('error', callback);
+
+      readStream.on('close', () => {
+        fs.unlinkSync(oldPath, callback);
+        resolve(filename);
+      });
+      readStream.pipe(writeStream);
+    });
+    console.log(`${link} file was added`);
+    listImgs.push(`http://localhost:7070/${link}`);
+    ctx.response.body = link;
+    return;
+  }
+
+  // DELETE
+  if (ctx.method === 'DELETE') {
+    const { file } = ctx.request.query;
+    const fileName = path.parse(file).base;
+    listImgs = listImgs.filter((item) => item !== file);
+    fs.unlink(`${imgFolder}/${fileName}`, (err) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      console.log(`${fileName} was deleted`);
+    });
+    ctx.response.body = 'ok';
+  }
+});
